75b9696cd0542e2fe602ad2cb776755f0a0c1933
thomas <thomasipod59122@gmail.com>
2023-04-12 15:56:46 +0200

    base

--- lib/main.dart
+++ lib/main.dart
@@ -34,29 +34,12 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Home Page'),
-      internapplication : const InternApplication(title: 'app') 
     );
   }
 }
 
 
-class InternApplication extends StatefulWidget {
-  const InternApplication({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<InternApplication> createState() => _InternApplicationState();
-}
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -122,26 +105,12 @@ class _MyHomePageState extends State<MyHomePage> {
                 decoration: const InputDecoration(
                 hintText: 'password',
               ),
-            ),          
+            ), 
+          
+                   
         ],
       ),
-
     );
-
-
-
   }
 }
 
-
-class _InternApplicationState extends State<InternApplication> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-
-    );
-  
-  }
-
-}
